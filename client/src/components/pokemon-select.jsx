256a70f90089fe844629643401e85a4c5e5ed53f
James Cox <j-cox03@hotmail.com>
2017-08-22 19:05:20 +0100

    Made request to assess pokemon names api

--- client/src/components/pokemon-select.jsx
+++ client/src/components/pokemon-select.jsx
@@ -2,7 +2,36 @@ import React from 'react';
 
 class PokemonSelect extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      pokemon: []
+    }
+  }
+
+  componentDidMount(){
+    const request = new XMLHttpRequest();
+    request.addEventListener("load", () => {
+        if(request.status !== 200) return;
+
+        const jsonString = request.responseText;
+        const pokemon = JSON.parse(jsonString);
+        this.setState({
+          pokemon: pokemon,
+        });
+      }
+    )
+    request.open("GET", "http://pokeapi.co/api/v2/pokemon/?limit=151&offset=0");
+    request.send();
+  }
+
   render(){
+
+
+    const pokemonOptions = pokemonObject.results.map((pokemon) => {
+      return <option></option>
+    });
+
     return (
       <p>Hello</p>
     );
